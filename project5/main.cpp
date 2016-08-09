eb94ba690a0361cdbbc59be0e1f927ed187f0a11
Ian Harris <ian+gh@dgby.org>
2016-08-08 20:37:20 -0700

    Initial Commit

---
+++ project5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project5
+//
+//  Created by Ian Harris on 16/8/8.
+//  Copyright © 2016 Ian Harris. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
